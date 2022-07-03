bbef7ec1cc82524bf49707e8ff5c79d30f54b929
dreywesson <dreywesson@gmail.com>
2022-07-03 12:09:58 +0100

    refactor numToWords_1.2

--- numToWords/index.js
+++ numToWords/index.js
@@ -1,40 +1,6 @@
+import { tens, hundreds } from "./utils.js";
+
 function numToWords(val) {
-  const tens = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten",
-    "eleven",
-    "twelve",
-    "thirteen",
-    "fourteen",
-    "fifteen",
-    "sixteen",
-    "seventeen",
-    "eighteen",
-    "nineteen",
-    "twenty",
-  ];
-  const hundreds = [
-    "zero",
-    "ten",
-    "twenty",
-    "thirty",
-    "forty",
-    "fifty",
-    "sixty",
-    "seventy",
-    "eighty",
-    "ninety",
-    "hundred",
-  ];
   let hundredWords = "",
     comma = "",
     result = "",
