2f72f2a1078c6a1d1f397d9884a00ddd8a7f91b3
dreywesson <dreywesson@gmail.com>
2022-08-17 11:12:19 +0100

    is rotation

--- strings/isRotation.js
+++ strings/isRotation.js
@@ -1,27 +1,28 @@
 function isRotation(str1, str2) {
-  const firstIdx = str2.charAt(0);
+  if (str1.length !== str2.length) return false;
+  let i = 0,
+    k = 0,
+    result = true;
+  const firstIdx = str2.charAt(0),
+    str1Len = str1.length,
+    tmp2 = str2.substr(1).length;
 
-  let i = 0;
-  for (i; i < str1.length; i++) if (firstIdx === str1.charAt(i)) break;
+  for (i; i < str1Len - 1; i++)
+    if (firstIdx === str1.charAt(i) && i + 1 === tmp2) break;
 
-  let k = 0;
-  let result = true;
-
-  for (let j = 0; j < str1.length; j++) {
-    if (str2.charAt(j) !== str1.charAt(j + i) && j + i < str1.length) {
+  console.log(i);
+  for (let j = 0; j < str1Len; j++) {
+    const condition1 = j + i < str1Len && str2.charAt(j) !== str1.charAt(j + i);
+    const condition2 =
+      i + j >= str1Len && str2.charAt(j) !== str1.charAt(k) && k < i;
+    if (condition1 || condition2) {
       result = false;
       break;
     }
-
-    if (i + j >= str1.length) {
-      if (str2.charAt(j) !== str1.charAt(k) && k < i) {
-        result = false;
-        break;
-      }
-      if (k >= i) break;
-      k++;
-    }
+    if (i + j >= str1Len) k++;
+    if (k >= i) break;
   }
   return result;
 }
-console.log(isRotation("ABCD", "DABC"));
+console.log(isRotation("ABBAB", "BABBA"));
+console.log(isRotation("ABBA", "BAAB"));
