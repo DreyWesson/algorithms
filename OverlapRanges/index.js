197943ed25e8d6ef2eff0522aacba988e8b41e8a
dreywesson <dreywesson@gmail.com>
2022-08-18 09:29:52 +0100

    dsa:range overlap

---
+++ OverlapRanges/index.js
@@ -0,0 +1,25 @@
+function overlapRanges(arr) {
+  const range = (min, max) => {
+    const newArr = [];
+    let init = min;
+    while (init < max) {
+      init = min++;
+      newArr.push(init);
+    }
+    return newArr;
+  };
+
+  const newArr = [...range(arr[0], arr[1]), ...range(arr[2], arr[3])];
+
+  const memo = {};
+  const tmp = [];
+  for (let i = 0; i < newArr.length; i++) {
+    const element = newArr[i];
+    memo[element] ? tmp.push(element) : (memo[element] = 1);
+  }
+  console.log(tmp);
+  return tmp.length >= arr[4] ? true : false;
+}
+
+console.log(overlapRanges([4, 10, 2, 6, 3]));
+console.log(overlapRanges([10, 20, 4, 14, 4]));
