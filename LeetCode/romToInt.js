1eb1bce8db97e4adb19d00019563d2f52ecd3dbd
dreywesson <dreywesson@gmail.com>
2022-08-26 12:42:12 +0100

    LeetCode: romToInt

--- LeetCode/romToInt.js
+++ LeetCode/romToInt.js
@@ -1,55 +1,28 @@
-var romToInt = function (nums) {
-  // find the length of nums
-  // split into 3s into an array
-  nums = "" + nums;
-  const arr = [];
-  let tmp = "";
-  let count = 0;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function char_to_int(c) {
   const dictionary = {
-    1: "I",
-    2: "II",
-    3: "III",
-    4: "IV",
-    5: "V",
-    6: "VI",
-    7: "VII",
-    8: "VIII",
-    9: "IX",
-    10: "X",
-    20: "XX",
-    30: "XXX",
-    40: "XL",
-    50: "L",
-    90: "XC",
-    100: "C",
-    200: "CC",
-    300: "CCC",
-    400: "CD",
-    500: "D",
-    900: "CM",
-    1000: "M",
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
   };
-  for (let i = nums.length - 1; i >= 0; i--) {
-    tmp = nums[i] + tmp;
-    count++;
-    if (count === 3) {
-      arr.push(tmp);
-      tmp = "";
-    }
+  return dictionary[c];
+}
+var romanToInt = function (str1) {
+  if (!str1) return -1;
+  let num = char_to_int(str1.charAt(0));
+  let pre, curr;
+  for (var i = 1; i < str1.length; i++) {
+    curr = char_to_int(str1.charAt(i));
+    pre = char_to_int(str1.charAt(i - 1));
+    if (curr <= pre) num += curr;
+    else num = num - pre * 2 + curr;
   }
-  if (tmp.length) arr.push(tmp);
-  console.log(arr);
-
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    const arrLen = element.length;
-
-    if (arrLen === 3) {
-      const leap = element.charAt(0);
-    } else if (arrLen === 2) {
-    } else {
-    }
-  }
+  return num;
 };
-
-romToInt(185);
