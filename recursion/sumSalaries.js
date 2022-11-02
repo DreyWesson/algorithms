91fce1cebb4debacc5d7bdd9ec455f7347a238d3
dreywesson <dreywesson@gmail.com>
2022-11-02 13:51:06 +0100

    recursion

---
+++ recursion/sumSalaries.js
@@ -0,0 +1,29 @@
+let company = {
+    // the same object, compressed for brevity
+    sales: [
+        { name: "John", salary: 1000 },
+        { name: "Alice", salary: 1600 },
+    ],
+    development: {
+        sites: [
+            { name: "Peter", salary: 2000 },
+            { name: "Alex", salary: 1800 },
+        ],
+        internals: [{ name: "Jack", salary: 1300 }],
+    },
+};
+
+// The function to do the job
+function sumSalaries(department) {
+    if (Array.isArray(department)) {
+        return department.reduce((prev, current) => prev + current.salary, 0);
+    } else {
+        let sum = 0;
+        for (let subdep of Object.values(department)) {
+            sum += sumSalaries(subdep);
+        }
+        return sum;
+    }
+}
+
+alert(sumSalaries(company));
