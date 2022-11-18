a014e20ce07bf77629d09239506532f149313a74
dreywesson <dreywesson@gmail.com>
2022-11-18 08:29:58 +0100

    LeetCode top interview questions: isValidSudoku,...

--- LeetCode/Top10InterviewQuestions/isValidSudoku.js
+++ LeetCode/Top10InterviewQuestions/isValidSudoku.js
@@ -68,25 +68,3 @@ let arr2 = [
     [".", ".", "4", ".", ".", ".", ".", ".", "."],
 ];
 console.log(isValidSudoku(arr));
-
-var isValidSudoku = function (board) {
-    const set = new Set();
-    // (m)v, v(n), (m)v(n)
-    for (let m = 0; m < board.length; m++) {
-        for (let n = 0; n < board[m].length; n++) {
-            const item = board[m][n];
-            if (item === ".") continue;
-            const row = `(${m})${item}`;
-            const col = `${item}(${n})`;
-            const rc = `(${~~(m / 3)})${item}(${~~(n / 3)})`;
-            if (!set.has(row) && !set.has(col) && !set.has(rc)) {
-                set.add(row);
-                set.add(col);
-                set.add(rc);
-            } else {
-                return false;
-            }
-        }
-    }
-    return true;
-};
